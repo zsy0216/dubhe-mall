011f277b9afca5a060a2e850450cc4ff288a3a5d
zsy0216 <594983498@qq.com>
2021-02-28 13:06:24 +0800

    feat: 初始化项目

---
+++ dubhe-api/src/main/java/com/zhang/DubheApplication.java
@@ -0,0 +1,15 @@
+package com.zhang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: zhangshuaiyin
+ * @date: 2021/2/28 13:01
+ */
+@SpringBootApplication
+public class DubheApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DubheApplication.class, args);
+    }
+}
